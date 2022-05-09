c00f0010cd91cee397183ba87611dd6bf9cd9c21
SirEikir <cristoferpuerto@gmail.com>
2022-05-09 14:27:31 +0200

    Ejecucion de Servelets con TOMCAT

--- src/main/java/es/avalon/HolaServlet.java
+++ src/main/java/es/avalon/HolaServlet.java
@@ -2,17 +2,27 @@ package es.avalon;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import es.avalon.activerecord.PersonaActiveRecord;
 
 public class HolaServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         PrintWriter pw = response.getWriter();
-        pw.println("hola desde un servlet");
+        List<PersonaActiveRecord> lista = PersonaActiveRecord.buscarTodos();
+        for (PersonaActiveRecord persona:lista){
+            pw.println("<p>"+persona.getDni()+"</p>");
+            pw.println("<p>"+persona.getNombre()+"</p>");
+            pw.println("<p>"+persona.getEdad()+"</p>");
+        }
+        
+       
         
     }
 
