cfcfa9541947275bb099742f63972bc67fa5df4e
SirEikir <cristoferpuerto@gmail.com>
2022-05-09 16:50:34 +0200

    Actualizacion de ServLet

--- src/main/java/es/avalon/HolaServlet.java
+++ src/main/java/es/avalon/HolaServlet.java
@@ -7,16 +7,17 @@ import java.util.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import es.avalon.activerecord.PersonaActiveRecord;
+import es.avalon.dominio.Persona;
+import es.avalon.repository.PersonaRepositoy;
 
 public class HolaServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         PrintWriter pw = response.getWriter();
-        List<PersonaActiveRecord> lista = PersonaActiveRecord.buscarTodos();
-        for (PersonaActiveRecord persona:lista){
+        PersonaRepositoy repositorio = new PersonaRepositoy();
+        List<Persona> lista = repositorio.buscarTodos();
+        for (Persona persona:lista){
             pw.println("<p>"+persona.getDni()+"</p>");
             pw.println("<p>"+persona.getNombre()+"</p>");
             pw.println("<p>"+persona.getEdad()+"</p>");
---
+++ src/main/java/es/avalon/dominio/Persona.java
@@ -0,0 +1,36 @@
+package es.avalon.dominio;
+
+public class Persona {
+    private String dni;
+    private String nombre;
+    private int edad;
+
+
+    public Persona(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+    
+}
